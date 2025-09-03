c3cc53756902b987e6e79e6dd72365f74a22a0e8
GazuaDev8417 <francadasilvaflamarion@gmail.com>
2025-09-03 01:09:30 -0300

    elimando restícios do swagger no projeto

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import paymentRoutes from './routes/paymentRoutes'
-import swaggerUi from 'swagger-ui-express'
-import swaggerDoc from './swagger.json'
  
 
 
@@ -11,12 +9,10 @@ export const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 app.use('/payments', paymentRoutes)
 
 
 
 app.listen(PORT, ()=>{ 
     console.log(`Servidor rodando em http://localhost:${PORT}`)
-    console.log(`Documentação disponível em http://localhost:${PORT}/api-docs`)
 })
